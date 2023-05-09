10b7dff70c0585e7f257cb0a4eccc5ac9835d345
mbagheri <mohammad.bagheri@oulu.fi>
2023-05-09 11:11:25 +0300

    add functions

---
+++ fcdimen/functions/welcome.py
@@ -0,0 +1,19 @@
+
+# logo made by http://www.network-science.de/ascii/
+def welcome_logo():
+    """Show package logo"""
+    print(
+      r""" 
+         _____ ____ ____  _                      
+        |  ___/ ___|  _ \(_)_ __ ___   ___ _ __  
+        | |_ | |   | | | | | '_ ` _ \ / _ \ '_ \ 
+        |  _|| |___| |_| | | | | | | |  __/ | | |
+        |_|   \____|____/|_|_| |_| |_|\___|_| |_|""")
+
+    print("""
+ *------------------------------------------------------*
+ |                FCDimen 0.1.0 (2023)                  |
+ |   Developers: M. Bagheri, E. Berger & H.-P. Komsa    |
+ |   Documentation : https://github.com/fcdimen         |
+ *------------------------------------------------------*
+ """)
